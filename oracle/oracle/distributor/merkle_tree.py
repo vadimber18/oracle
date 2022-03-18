9106025f2f6894485b93f139939ac8c70dbb57e5
Vadim Bereznitskiy <vadimber1994@mail.ru>
2022-03-18 23:26:23 +0300

    Add faster version of Merkle tree

--- oracle/oracle/distributor/merkle_tree.py
+++ oracle/oracle/distributor/merkle_tree.py
@@ -1,4 +1,4 @@
-from collections import OrderedDict
+import collections
 from typing import Dict, List, Tuple, Union
 
 from eth_typing import ChecksumAddress
@@ -11,90 +11,89 @@ from .types import Claim, Claims, Rewards
 w3 = Web3()
 
 
-# Inspired by https://github.com/Uniswap/merkle-distributor/blob/master/src/merkle-tree.ts
-class MerkleTree(object):
+class Node:
+    """MerkleTree Node."""
+
+    __slots__ = ("value", "pair", "parent")
+
+    def __init__(self, value, pair=None, parent=None):
+        self.value = value
+        self.pair = pair
+        self.parent = parent
+
+    def __repr__(self):
+        return f"Node {self.value}"
+
+
+class MerkleTree:
     def __init__(self, elements: List[bytes]):
-        self.elements: List[bytes] = sorted(list(set(elements)))
-        self.element_positions: Dict[bytes, int] = {
-            el: index for index, el in enumerate(self.elements)
-        }
+        self.leaves: Dict[bytes, Node] = dict()
+        self.root = None
+        self.fill_leaves(elements)
+        self.fill_tree()
 
-        # create layers
-        self.layers: List[List[bytes]] = self.get_layers(self.elements)
+    def fill_leaves(self, elements: List[bytes]):
+        prev: Union[Node, None] = None
+        for element in sorted(set(elements)):
+            node = Node(element)
+            self.leaves[element] = node
+            if prev:
+                prev.pair = node
+                node.pair = prev
+                prev = None
+            else:
+                prev = node
 
-    def get_layers(self, elements: List[bytes]) -> List[List[bytes]]:
-        if not elements:
-            raise ValueError("Empty tree")
+    def fill_tree(self):
+        current_level: List[Node] = list(self.leaves.values())
 
-        layers = [elements]
-
-        # get next layer until we reach the root
-        while len(layers[-1]) > 1:
-            layers.append(self.get_next_layer(layers[-1]))
-
-        return layers
-
-    def get_root(self) -> bytes:
-        return self.layers[-1][0]
-
-    def get_hex_root(self) -> HexStr:
-        return w3.toHex(self.get_root())
+        level_length = len(current_level)
+        while level_length > 1:
+            next_level: List[Node] = []
+            index = 0
+            while index < level_length:
+                node = current_level[index]
+                if node.pair:
+                    parent = Node(MerkleTree.combine_hash(node.value, node.pair.value))
+                    node.pair.parent = parent
+                    # increase index cause we already covered two nodes
+                    index += 1
+                else:
+                    parent = Node(node.value)
+                node.parent = parent
+                # if prev node has no pair - update it
+                if next_level and not next_level[-1].pair:
+                    next_level[-1].pair = parent
+                    parent.pair = next_level[-1]
+                next_level.append(parent)
+                index += 1
+            current_level = next_level
+            level_length = len(current_level)
+        self.root = current_level[0]
 
     def get_proof(self, element: bytes) -> List[bytes]:
-        index = self.element_positions.get(element, None)
-        if index is None:
-            raise ValueError("Element is not in Merkle Tree")
-
+        node: Node = self.leaves.get(element)
+        if not node:
+            raise ValueError("No such element in Merkle Tree")
         proof: List[bytes] = []
-        for layer in self.layers:
-            pair_element = MerkleTree.get_pair_element(index, layer)
-            if pair_element:
-                proof.append(pair_element)
-
-            index = index // 2
-
+        while node:
+            if node.pair:
+                proof.append(node.pair.value)
+            node = node.parent
         return proof
 
     def get_hex_proof(self, element: bytes) -> List[HexStr]:
-        proof = self.get_proof(element)
-        return [w3.toHex(p) for p in proof]
+        return [w3.toHex(p) for p in self.get_proof(element)]
+
+    @property
+    def hex_root(self) -> HexStr:
+        return w3.toHex(self.root.value)
 
     @staticmethod
-    def get_next_layer(elements: List[bytes]) -> List[bytes]:
-        next_layer: List[bytes] = []
-        for i, el in enumerate(elements):
-            if i % 2 == 0:
-                # Hash the current element with its pair element
-                if i < len(elements) - 1:
-                    combined_hash = MerkleTree.combine_hash(el, elements[i + 1])
-                else:
-                    combined_hash = el
-
-                next_layer.append(combined_hash)
-
-        return next_layer
-
-    @staticmethod
-    def combine_hash(first: bytes, second: bytes) -> bytes:
-        if not first:
-            return second
-
-        if not second:
-            return first
-
-        return keccak(primitive=b"".join(sorted([first, second])))
-
-    @staticmethod
-    def get_pair_element(index: int, layer: List[bytes]) -> Union[bytes, None]:
-        if index % 2 == 0:
-            pair_index = index + 1
-        else:
-            pair_index = index - 1
-
-        if pair_index < len(layer):
-            return layer[pair_index]
-
-        return None
+    def combine_hash(first: bytes, second: bytes = None) -> bytes:
+        if first and second:
+            return keccak(primitive=b"".join(sorted([first, second])))
+        return first or second
 
 
 def get_merkle_node(
@@ -114,17 +113,17 @@ def get_merkle_node(
 def calculate_merkle_root(rewards: Rewards) -> Tuple[HexStr, Claims]:
     """Calculates merkle root and claims for the rewards."""
     merkle_elements: List[bytes] = []
-    accounts: List[ChecksumAddress] = sorted(rewards.keys())
-    claims: Claims = OrderedDict()
-    for i, account in enumerate(accounts):
+    accounts: List[ChecksumAddress] = sorted(rewards)
+    claims: Claims = dict()
+    for index, account in enumerate(accounts):
         tokens: List[ChecksumAddress] = sorted(rewards[account].keys())
-        claim: Claim = OrderedDict(
-            index=i, tokens=tokens, values=[rewards[account][t] for t in tokens]
+        claim: Claim = collections.OrderedDict(
+            index=index, tokens=tokens, values=[rewards[account][t] for t in tokens]
         )
         claims[account] = claim
 
         merkle_element: bytes = get_merkle_node(
-            index=i,
+            index=index,
             account=account,
             tokens=tokens,
             values=[int(val) for val in claim["values"]],
@@ -134,11 +133,11 @@ def calculate_merkle_root(rewards: Rewards) -> Tuple[HexStr, Claims]:
     merkle_tree = MerkleTree(merkle_elements)
 
     # collect proofs
-    for i, account in enumerate(accounts):
-        proof: List[HexStr] = merkle_tree.get_hex_proof(merkle_elements[i])
+    for index, account in enumerate(accounts):
+        proof: List[HexStr] = merkle_tree.get_hex_proof(merkle_elements[index])
         claims[account]["proof"] = proof
 
     # calculate merkle root
-    merkle_root: HexStr = merkle_tree.get_hex_root()
+    merkle_root: HexStr = merkle_tree.hex_root
 
     return merkle_root, claims
